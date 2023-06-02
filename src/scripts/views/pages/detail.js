4c2b14dbd0930c20bb120530376d4333e93a562c
Anggaranii <116339201+Anggaranii@users.noreply.github.com>
2023-06-02 13:21:08 +0700

    Add files via upload

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,36 @@
+import UrlParser from '../../routes/url-parser';
+import RestaurantDbSource from '../../data/restaurantdb-source';
+import { createRestaurantDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+
+const Detail = {
+  async render() {
+    return `
+      <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
+    `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await RestaurantDbSource.detailRestaurant(url.id);
+    console.log(restaurant);
+    const restaurantContainer = document.querySelector('#restaurant');
+    restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        description: restaurant.description,
+        city: restaurant.city,
+        address: restaurant.address,
+        pictureId: restaurant.pictureId,
+        rating: restaurant.rating,
+      },
+    });
+  },
+};
+
+export default Detail;
